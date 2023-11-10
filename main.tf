007c21f76e1e9c23e7ba378c7d0a85459157b82e
Richard Nobes <richard.nobes@corsearch.com>
2023-11-10 18:46:06 +0000

    Added ASG and ALB

--- main.tf
+++ main.tf
@@ -149,14 +149,14 @@ resource "aws_instance" "ec2_sub1C" {
   
 resource "aws_launch_template" "ecs_lt" {
  name_prefix   = "ecs-template"
- image_id      = "ami-062c116e449466e7f"
+ image_id      = "ami-076c7acfc9e8ee57d"
  instance_type = "t3.micro"
 
- key_name               = "ec2ecsglog"
+#  key_name               = "ec2ecsglog"
  vpc_security_group_ids = [aws_security_group.My_VPC_Security_Group.id]
- iam_instance_profile {
+/*  iam_instance_profile {
    name = "ecsInstanceRole"
- }
+ } */
 
  block_device_mappings {
    device_name = "/dev/xvda"
@@ -174,4 +174,57 @@ resource "aws_launch_template" "ecs_lt" {
  }
 
  user_data = filebase64("${path.module}/ecs.sh")
+}
+
+resource "aws_autoscaling_group" "ecs_asg" {
+ vpc_zone_identifier = [aws_subnet.subnet1A.id, aws_subnet.subnet1B.id, aws_subnet.subnet1C.id]
+ desired_capacity    = 3
+ max_size            = 5
+ min_size            = 1
+
+ launch_template {
+   id      = aws_launch_template.ecs_lt.id
+   version = "$Latest"
+ }
+
+ tag {
+   key                 = "AmazonECSManaged"
+   value               = true
+   propagate_at_launch = true
+ }
+}
+
+resource "aws_lb" "ecs_alb" {
+ name               = "ecs-alb"
+ internal           = false
+ load_balancer_type = "application"
+ security_groups    = [aws_security_group.My_VPC_Security_Group.id]
+ subnets            = [aws_subnet.subnet1A.id, aws_subnet.subnet1B.id, aws_subnet.subnet1C.id]
+
+ tags = {
+   Name = "ecs-alb"
+ }
+}
+
+resource "aws_lb_listener" "ecs_alb_listener" {
+ load_balancer_arn = aws_lb.ecs_alb.arn
+ port              = 80
+ protocol          = "HTTP"
+
+ default_action {
+   type             = "forward"
+   target_group_arn = aws_lb_target_group.ecs_tg.arn
+ }
+}
+
+resource "aws_lb_target_group" "ecs_tg" {
+ name        = "ecs-target-group"
+ port        = 80
+ protocol    = "HTTP"
+ target_type = "ip"
+ vpc_id      = aws_vpc.main.id
+
+ health_check {
+   path = "/"
+ }
 }
